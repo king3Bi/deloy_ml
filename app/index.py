5dcfcf685983d33eed657d621412aac670695f70
king3Bi <vovanhauspkt@gmail.com>
2021-11-28 17:38:55 +0700

    update msg result

--- app/index.py
+++ app/index.py
@@ -31,12 +31,14 @@ def predict():
     if result[0] == 0:
         return jsonify({
             'code': 0,
-            'msg': 'Không có khả năng'
+            'msg': 'Bạn có một sức khỏe rất tốt, hãy cố gắng duy trì'
         })
     elif result[0] == 1:
         return jsonify({
             'code': 1,
-            'msg': 'Có khả năng'
+            'msg': 'Kết quả là có nguy cơ, '
+                   'nhưng bạn đừng lo lắng, hãy bỏ những thói quen xấu ảnh hưởng tới sức khỏe, '
+                   'luyện tập thể dục để có sức khỏe tốt'
         })
     else:
         return jsonify({
